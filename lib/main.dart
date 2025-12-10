ca1b016377025bc794459eb73edc78c62e2bba2f
kassim alekaby <34183201+kassimkaher@users.noreply.github.com>
2025-12-10 17:06:54 +0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'config/store_app.dart';
 import 'core/utils/app_credentials.dart';
@@ -18,7 +19,7 @@ void main() async {
     enableLogging: true,
     requestTimeoutSeconds: 30,
   );
-
+  setPathUrlStrategy();
   // Setup dependency injection
   await registerDependencies(storeConfig);
 
